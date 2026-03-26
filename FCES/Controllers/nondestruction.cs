884033a8c6fb1d5a3819a0edc3328dd1362b504b
Babita Kanojiya <babitakanojiya98@gmail.com>
2026-03-26 17:36:54 +0530

    add more module

---
+++ FCES/Controllers/nondestruction.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FCES.Controllers
+{
+    public class nondestruction : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FCES/Controllers/trainingandcertification.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FCES.Controllers
+{
+    public class trainingandcertification : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
