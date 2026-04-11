86719a375445ede9430786e4b167946546af6128
Babita Kanojiya <babitakanojiya98@gmail.com>
2026-04-11 17:48:14 +0530

    add video

---
+++ FCES/Models/CourseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FCES.Models
+{
+    public class CourseModel
+    {
+       
+            public string ParentCourse { get; set; }
+            public string ChildCourse { get; set; }
+            public string VideoUrl { get; set; }
+        
+    }
+}
